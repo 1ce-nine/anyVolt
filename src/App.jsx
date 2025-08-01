92b78c6a960858ee05102e7021cc4b6ed98c2471
Edward Still <stier001@mymail.unisa.edu.au>
2025-08-01 12:37:25 +1000

    Further testing

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <>
       {/* This is a React Fragment. It doesn't add an extra div to the DOM. */}
-      <h1>Anyvolt</h1>
+      <h1>Anyvolt Placeholder Page</h1>
       <p></p>
     </>
   );
