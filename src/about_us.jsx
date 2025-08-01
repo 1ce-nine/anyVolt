f1f8b3d6af45dca13d910310b2ccee16495cb23c
Edward Still <stier001@mymail.unisa.edu.au>
2025-08-01 16:59:25 +1000

    Created link back to home page from about us page. also created header and footer functions for about us page

--- src/about_us.jsx
+++ src/about_us.jsx
@@ -1,16 +1,37 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
+function Header() {
+  return(
+    <header>
+      About Us
+    </header>
+  )
+}
+
+function Footer() {
+  return (
+    <footer>
+      <Link to="/">Back to Home</Link>
+    </footer>
+  )
+}
 
 function AboutUs() {
   return (
     <>
       <main>
+        <Header />
         <ul>
-          <li>Contact Number</li>
-          <li>Email Address</li>
+          <li>History</li>
+          <li>Mission</li>
         </ul>
+        <Footer />
       </main>
     </>
   );
 }
+
+
 
 export default AboutUs;
