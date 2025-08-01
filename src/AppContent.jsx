a8908c1f6041dbea6dfa9aff43cdcdf479dab947
Edward Still <stier001@mymail.unisa.edu.au>
2025-08-01 17:29:48 +1000

    Testing bootstrap out in environment

--- src/AppContent.jsx
+++ src/AppContent.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Container, Row, Col } from 'react-bootstrap';
 
 function AppContent() {
   return (
     <>
-      {/* This is a React Fragment. It doesn't add an extra div to the DOM. */}
-      <h1>Anyvolt Placeholder Page</h1>
-        <p>
-            <Link to="/about-us">About us!</Link>
-        </p>
+      <Container>
+        <Row>
+          <Col>
+            <h1>Anyvolt Placeholder Page</h1>
+          </Col>
+        </Row>
+      </Container>
+      <p>
+        <Link to="/about-us">About us!</Link>
+      </p>
     </>
   );
 }
