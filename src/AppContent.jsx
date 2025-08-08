79b151e2879ef8b80061b0fbef57dca356396eb2
Edward Still <stier001@mymail.unisa.edu.au>
2025-08-08 16:47:25 +1000

    Added not-yet functional buttons for login and sign up and styled them in-line. Also restructured the importing of the css to the html file

--- src/AppContent.jsx
+++ src/AppContent.jsx
@@ -1,17 +1,32 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col, Button } from 'react-bootstrap';
 
 function AppContent() {
   return (
     <>
       <title>Anyvolt</title>
         <Container>
-          <Row >
-            /* Centre the text */
-            <Col className="text-center">
-              <h1 style={{color: 'white'}}>Anyvolt</h1>
+          <Row className="align-items-center header-row">
+            <Col xs={4}>
+              <h1 className="header-title" style={{color: 'white'}}>Anyvolt</h1>
             </Col>
+
+            <Col xs={6} className="d-flex justify-content-end header buttons">
+              <Button style={{
+                  backgroundColor: '#3f1e63',
+                  border: 'none',
+                  color: 'white',
+                  marginRight: '0.5rem',
+                }}>Login</Button>
+
+              <Button style={{
+                  backgroundColor: 'transparent',
+                  borderColor: 'white',
+                  color: 'white',}}
+                >Sign up</Button>            
+            </Col>
+
           </Row>
         </Container>
         <p>
