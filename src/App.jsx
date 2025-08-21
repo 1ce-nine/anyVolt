592bb82173ab29407354e02134aabf193afb549d
Edward Still <stier001@mymail.unisa.edu.au>
2025-08-21 18:41:36 +1000

    Add 'glowing orbs' structure to css and added divs to structure the css into the App.jsx file to render them correctly. Orb placement and colouring is still to be confirmed

--- src/App.jsx
+++ src/App.jsx
@@ -10,19 +10,27 @@ import LogInPage from './LoginPage.jsx';
 
 function App() {
   return (
-    <div className='app-wrapper'>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<AppContent/>} />
-          <Route path="/about-us" element={<AboutUs />} />
-          <Route path="/services" element={<Services />} />
-          <Route path="/products" element={<Products />} />
-          <Route path="/investors" element={<Investors />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/news" element={<News />} />
-          <Route path="/loginpage" element={<LogInPage />} />
-        </Routes>
-      </BrowserRouter>
+    <div className='root-wrapper'>
+      {/* These divs will be your glowing orbs */}
+      <div className="orb orb-purple"></div>
+      <div className="orb orb-aqua-1"></div>
+      <div className="orb orb-aqua-2"></div>
+      <div className="orb orb-dark-1"></div>
+
+      <div className="app-wrapper">
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<AppContent/>} />
+            <Route path="/about-us" element={<AboutUs />} />
+            <Route path="/services" element={<Services />} />
+            <Route path="/products" element={<Products />} />
+            <Route path="/investors" element={<Investors />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/news" element={<News />} />
+            <Route path="/loginpage" element={<LogInPage />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
     </div>
   );
 }
