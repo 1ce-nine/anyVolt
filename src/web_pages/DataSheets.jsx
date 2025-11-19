53524368488b2f0a8c235accde0b15c95e63e62c
Edward Still <stier001@mymail.unisa.edu.au>
2025-11-19 13:26:21 +1000

    Added sample data sheet downlable PDF

--- src/web_pages/DataSheets.jsx
+++ src/web_pages/DataSheets.jsx
@@ -1,8 +1,7 @@
 {/* */}
 {/* Import necessary libraries, components etc */}
 import React from "react";
-import { Link } from "react-router-dom";
-import { Container, Row, Col, Accordion, Card } from 'react-bootstrap';
+import { Container, Row, Col } from "react-bootstrap";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import Breadcrumbs from "../components/Breadcrumbs";
@@ -11,27 +10,43 @@ function DataSheets() {
   return (
     <>
       <title>Data Sheets</title>
-        <Header />
-        <main>  
-          <Container>
-          <Breadcrumbs labels={{
-          investors: "Investors",
-          "datasheets": "Data Sheets"
-          }} />
-            <h1 className="page-heading anyvolt-logo button-transparent" style={{fontSize: '50px'}}>Data Sheets</h1>
-            <Row className="mt-5">
-              <Col md={10} style={{color: 'white'}}>
-                {/* Placeholder */}
-                <h3 className="button-transparent">
-                  Coming soon...
-                </h3>    
-              </Col>
-            </Row>
-          </Container>
-        </main>
-        <Footer />
+      <Header />
+      <main>
+        <Container>
+          <Breadcrumbs
+            labels={{
+              investors: "Investors",
+              datasheets: "Data Sheets",
+            }}
+          />
 
+          <h1
+            className="page-heading anyvolt-logo button-transparent"
+            style={{ fontSize: "50px" }}
+          >
+            Data Sheets
+          </h1>
+
+          <Row className="mt-5">
+            <Col md={10} style={{ color: "white" }}>
+              <h3 className="button-transparent">Coming soon...</h3>
+
+              {/* ✔ Download button */}
+              <a
+                href="/sample-datasheet.pdf"
+                download
+                className="btn btn-primary purple-style-button"
+                style={{ marginTop: "15px", display: "inline-block" }}
+              >
+                Download Sample PDF
+              </a>
+            </Col>
+          </Row>
+        </Container>
+      </main>
+      <Footer />
     </>
   );
 }
+
 export default DataSheets;
